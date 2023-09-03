fc26113aba0db48e325425ec90094227a4fe7ca7
zzjc1234 <2359047351@qq.com>
2023-09-03 11:12:11 +0800

    fix: format the code

--- start.py
+++ start.py
@@ -7,21 +7,21 @@ pt.append(root_path)
 
 name_app = path.basename(__file__)[0:-3]  # Get the name of the script
 log_config = {
-"version": 1,
-"disable_existing_loggers": True,
-"handlers": {
-    "file_handler": {
-        "class": "logging.FileHandler",
-        "filename": "logfile.log",
+    "version": 1,
+    "disable_existing_loggers": True,
+    "handlers": {
+        "file_handler": {
+            "class": "logging.FileHandler",
+            "filename": "logfile.log",
+        },
     },
-},
-"root": {
-    "handlers": ["file_handler"],
-    "level": "INFO",
-},
-"root": {
-    "handlers": ["file_handler"],
-    "level": "INFO",
-},
+    "root": {
+        "handlers": ["file_handler"],
+        "level": "INFO",
+    },
+    "root": {
+        "handlers": ["file_handler"],
+        "level": "INFO",
+    },
 }
-uvicorn.run(f"canvas_app:app", port=9283, reload=False,log_config=log_config)
+uvicorn.run(f"canvas_app:app", port=9283, reload=False, log_config=log_config)
